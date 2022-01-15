dec03bf9693c184639042095eeeddc8e8a11deb6
Demitrius <demitrius.quadros@outlook.com>
2022-01-15 17:26:33 -0300

    Continuidade da aula

--- LacosRepeticao/Program.cs
+++ LacosRepeticao/Program.cs
@@ -6,8 +6,29 @@ namespace LacosRepeticao
     {
         public static void Main(string[] args)
         {
+            //int n = 1;
+            //while (n <= 10)
+            //{
+            //    Console.WriteLine(String.Format("Valor corrente de n é: {0}", n));
+            //    n++;
+            //}
+            //Console.ReadKey();
 
+            //int n = 1;
+            //do
+            //{
+            //    Console.WriteLine("Valor de n é: " + n);
+            //    n++;
+            //} while (n >= 10);
+            //Console.ReadKey();
 
+         
+            int[] arrayDeInteiros = new int[] { 0, 1, 1, 2, 3, 5, 8, 13 };
+            foreach (int inteiro in arrayDeInteiros)
+            {
+                Console.WriteLine(inteiro);
+            }
+            Console.Read();
 
         }
     }
