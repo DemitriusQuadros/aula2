33d1829fba6a5cfe3143c668959b63badd6e86f6
Demitrius <demitrius.quadros@outlook.com>
2022-01-15 11:09:09 -0300

    Meu primeiro commit

---
+++ Aula2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Aula2
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
